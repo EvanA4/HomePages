48b14bf5e39beea2799546aeb89e4209b4c977dc
Evan Abbott <107959883+EvanA4@users.noreply.github.com>
2024-07-14 20:29:42 -0400

    trying to figure out post-processing

--- client/app/components/greeting.tsx
+++ client/app/components/greeting.tsx
@@ -1,27 +1,105 @@
 'use client'
-import React from 'react'
-import { Canvas, extend, useLoader } from '@react-three/fiber'
-import { SphereGeometry, ShaderMaterial, TextureLoader, Vector3 } from 'three'
+import React, { useEffect, useRef } from 'react'
+import { Canvas, extend, useFrame, useLoader, useThree } from '@react-three/fiber'
+import * as THREE from 'three'
 import { OrbitControls } from '@react-three/drei'
-extend({ SphereGeometry, ShaderMaterial, TextureLoader })
 
 
 // The below four lines are normally red, idk how to fix this yet -- still works!
 import planetVert from './shaders/planetVert.glsl'
 import planetFrag from './shaders/planetFrag.glsl'
-import atmVert from './shaders/atmVert.glsl'
-import atmFrag from './shaders/atmFrag.glsl'
+// import atmVert from './shaders/atmVert.glsl'
+// import atmFrag from './shaders/atmFrag.glsl'
 import pngTexture from '../../public/marsColors.png'
+import { EffectComposer, ShaderPass, RenderPass } from 'three/examples/jsm/Addons.js'
+extend({ EffectComposer, ShaderPass, RenderPass })
+
+
+const atmPost = {
+  uniforms: {
+      depthTexture: { value: null}
+  },
+  vertexShader: `
+    uniform sampler2D depthTexture;
+    varying sampler2D vdepthTxt;
+    varying vec2 vUv;
+
+    void main () {
+      vdepthTxt = depthTexture;
+      vUv = uv;
+
+      gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    }        
+  `,
+  fragmentShader: `
+    varying sampler2D vdepthTxt;
+    varying vec2 vUv;
+
+    void main() {
+      float depth = texture2D(vdepthTxt, vUv);
+      gl_FragColor = vec4(depth, depth, depth, 1.0); boogie woogie
+    }
+  `
+}
+
+
+const PostFX = () => {
+  const effectRef = useRef<EffectComposer>(null!)
+  const shaderRef = useRef<ShaderPass>(null!)
+  const { gl, size, scene, camera } = useThree()
+
+
+  const [ target ] = React.useMemo(() => {
+    const target = new THREE.WebGLRenderTarget(
+        size.width,
+        size.height,
+        {
+            minFilter: THREE.LinearFilter,
+            magFilter: THREE.LinearFilter,
+            format: THREE.RGBFormat,
+            stencilBuffer: false,
+            depthBuffer: true,
+            depthTexture: new THREE.DepthTexture(size.width, size.height)
+        },
+    );
+    return [ target ];
+  }, []);
+
+
+  useEffect(() => {
+    effectRef.current.setSize(size.width, size.height)
+  }, [size])
+
+
+  useFrame((state) => {
+    // First pass, get the depth texture
+    state.gl.setRenderTarget(target)
+    state.gl.render(scene, camera)
+
+    shaderRef.current.material.uniforms.depthTexture.value = target.depthTexture;
+
+    // Second pass, render normal stuff
+    effectRef.current.render()
+  }, 1)
+
+
+  return (
+    <effectComposer ref={effectRef} args={[gl]}>
+      <renderPass attachArray="passes" scene={scene} camera={camera} />
+      <shaderPass attachArray="passes" ref={shaderRef} args={[atmPost]} needsSwap={false} renderToScreen ></shaderPass>
+    </effectComposer>
+  )
+}
 
 
 const MyPlanet = () => {
-  const colorMap = useLoader(TextureLoader, pngTexture.src)
-  const lightDir = new Vector3(-10, 4, 10)
+  const planetRef = useRef<THREE.Mesh>(null!)
+  const colorMap = useLoader(THREE.TextureLoader, pngTexture.src)
+  const lightDir = new THREE.Vector3(-10, 4, 10)
 
   return (
     <>
-      {/* The raw planet texture */}
-      <mesh>
+      <mesh ref={planetRef}>
         <sphereGeometry args={[1, 32, 32]}/>
         <shaderMaterial
           uniforms={{
@@ -30,19 +108,6 @@ const MyPlanet = () => {
           }}
           vertexShader={planetVert}
           fragmentShader={planetFrag}
-        />
-      </mesh>
-
-      {/* The fancy atmosphere */}
-      <mesh>
-        <sphereGeometry args={[2, 32, 32]}/>
-        <shaderMaterial
-          uniforms={{
-            lightDir: { value: lightDir}
-          }}
-          vertexShader={atmVert}
-          fragmentShader={atmFrag}
-          transparent={true}
         />
       </mesh>
     </>
@@ -55,10 +120,9 @@ const Greeting = () => {
   return (
     <div className='flex justify-center items-center h-[80vh] bg-[#000000]'>
       <Canvas>
-        <mesh>
-          <MyPlanet/>
-        </mesh>
+        <MyPlanet/>
         <OrbitControls/>
+        <PostFX/>
       </Canvas>
     </div>
   )
--- client/app/components/shaders/atmFrag.glsl
+++ client/app/components/shaders/atmFrag.glsl
@@ -1,8 +1,12 @@
 // most uniforms and attributes are already provided by ThreeJS
 // see docs at https://threejs.org/docs/#api/en/renderers/webgl/WebGLProgram
 // also helpful code https://tympanus.net/codrops/2024/07/09/creating-an-animated-displaced-sphere-with-a-custom-three-js-material/
+// depth texture codes at https://github.com/mrdoob/three.js/blob/70cc4e192fe2ebd0bf8542a81c8c513d61984c58/examples/webgl_depth_texture.html
+// and https://discourse.threejs.org/t/reading-from-depth-texture/51344
+// and docs at https://threejs.org/docs/?q=depth#api/en/textures/DepthTexture
 
 uniform vec3 lightDir;
+uniform sampler2D depthTexture;
 varying vec3 vPos;
 varying vec3 vNorm;
 varying vec3 vCol;
@@ -10,19 +14,11 @@ varying vec2 vUv;
 
 
 void main() {
-    // Ray myRay = 1;
+    
+    gl_FragColor = texture2D(depthTexture, vUv);
 
-    // vec3 color = vCol * dot(vNorm, );
-
-    // gl_FragColor = vec4(normalize(vPos), 1.);
-
-    // gl_FragColor = vec4(vUv, 0., 1.);
-
-    // float dist = distance(vPos, cameraPosition);
-    // gl_FragColor = vec4(dist, dist, dist, 1.);
-
-    float camDot = dot(vNorm, cameraPosition) / length(cameraPosition);
-    float lightDot = dot(vNorm, lightDir) / length(lightDir);
-    float outFloat = camDot * lightDot;
-    gl_FragColor = vec4(outFloat, outFloat, outFloat, 0.1);
+    // float camDot = dot(vNorm, cameraPosition) / length(cameraPosition);
+    // float lightDot = dot(vNorm, lightDir) / length(lightDir);
+    // float outFloat = camDot * lightDot;
+    // gl_FragColor = vec4(outFloat, outFloat, outFloat, 0.1);
 }
