54eed6de31bf6f0172f2a3be4b09b2696e42efad
Evan Abbott <107959883+EvanA4@users.noreply.github.com>
2024-09-14 13:54:06 -0400

    fixed lists in blogs

--- client/app/blogs/testing/page.tsx
+++ client/app/blogs/testing/page.tsx
@@ -48,6 +48,11 @@ function blogTSX(props: any) {
             {props.children}
         </div>
     )}
+    const Elist = (props: any) => { return (
+        <ul className="list-disc pl-[20px]">
+            {props.children}
+        </ul>
+    )}
     const H1 = (props: any) => {
         return (
             <div className='mt-[40px] mb-[20px]'>
@@ -89,33 +94,33 @@ function blogTSX(props: any) {
                 src='/blogPics/simPicCrop.png'
                 height={687}
                 width={683}
-                alt='My Fragment Shader'
+                alt='My fragment shader'
             >
                 The atmosphere and postprocessing fragment shader.
             </IMG>
             <p>
                 To the uninitiated, shaders are pretty much magic. Want a cube to look like a sphere? Vertex shader. Want to simulate inconceivable quantities
-                of particles for a fluid simulation? Compute shader. In this blog, I'll be explaining the inner workings of my fragment shader for the atmosphere
-                and how it fits in the context of the rest of my ThreeJS scene.
+                of particles for a fluid simulation? Compute shader. In this blog, I'll be explaining what shaders even are. I'll also discuss the inner workings of
+                my fragment shader for the atmosphere and how it fits in the context of the rest of my ThreeJS scene.
             </p>
             <Tip>If you're unsure what to call a sphere that is technically a cube, one of my friends called it a <Code>sphube</Code>.</Tip>
             <H2>What is a shader?</H2>
             <p>
-                In my opinion, it's easiest to learn what shaders are by analyzing the types of shaders. In general, a shader is just some code than runs on a GPU,
-                normally in parallel.
+                Very generally, a shader is just some code than runs in parallel on a GPU. Here are some of the different types:
             </p>
             <H3>Vertex Shader</H3>
             <p>
-                A vertex shader runs in parallel for each vertex on an object you wish to render. All it computes is where the vertex should appear on the screen, but
-                messing with this shader can yield <a className='text-blue-400' href="https://github.com/kulesz/PlanetaryWater"><u>cool distortions</u></a> of a mesh.
+                Suppose you want to render an object. All objects in the rendering pipeline are comprised of vertices and edges. The vertex shader just computes where each vertex
+                should appear on the screen, but messing with this shader can yield <a className='text-blue-400' href="https://github.com/kulesz/PlanetaryWater"><u>cool
+                distortions</u></a> of a mesh.
             </p>
             <H3>Fragment Shader</H3>
             <p>
-                A fragment shader, runs for each pixel said object takes up on the screen. This code ultimately determines what the color of that pixel shoud be. Textures
-                are a common input here.
+                Rendering this object also requires a fragment shader. This shader runs for each pixel the object takes up on the screen. This code ultimately determines what color
+                of a pixel should be. Textures are a common input here.
             </p>
             <Important>
-                Just to reiterate, both vertex shaders and fragment shaders must be associated with an object!
+                Both vertex shaders and fragment shaders are <i>always</i> associated with an object!
             </Important>
             <H3>Compute Shader</H3>
             <p>
@@ -124,25 +129,33 @@ function blogTSX(props: any) {
             </p>
             <H2>My Shader's Context</H2>
             <p>
-                In my scene, I told ThreeJS (it's technically React Three Fiber since this is a React site) to render the scene <i>twice</i>. The first time, the scene
-                is rendered without my atmosphere around the planet. The rendered frame is then given to my fragment shader. But what object is my fragment shader for?
+                My shader is responsible for displaying the atmosphere on my home page, and I just said that a fragment shader must be associated with an object. So what object
+                does my fragment shader describe the pixels of? It's not the atmosphere.
             </p>
-            <H3>The Atmosphere "Mesh"</H3>
+            <H3>The Atmosphere "Object"</H3>
             <p>
-                I said that fragment shaders run for a specific object, and my shader is no exception. Since my fragment shader is for postprocessing, the mesh is actually
-                a rectangle (tecnically a plane) that <i>perfectly</i> covers the camera. All my shader does is take the rendered frame from earlier and draw in the atmosphere
-                on top. This frame is then drawn on top of the rectangle with my fragment shader. It's like if you asked me to draw a portait of you and instead I draw a
-                beautiful landscape. However, just before I turn the drawing towards you, I realize you asked for a portait and I draw in a shoddy stick figure on top.
+                Since my fragment shader is for postprocessing, the mesh is actually a rectangle (tecnically a plane) that <i>perfectly</i> covers the camera.
                 <br/><br/>
-                The scene then renders the rectangle, and because the rectangle perfectly fits the camera, the artificial scene looks like the real one.
+                My shader has two steps:
             </p>
-            <H2>Simulating the Light</H2>
-            <H3>1. Backwards Ray Tracing</H3>
+            <Elist>
+                <li>Take the pixel that is below the rectangle</li>
+                <li>Add any necessary atmospheric coloring</li>
+                <li>Display that to the screen</li>
+            </Elist>
+            <br/>
+            <p>    
+                It's like if you asked me to draw a portait of you and instead I draw a
+                beautiful landscape. However, just before I turn the drawing towards you, I realize you asked for a portait and I draw in a shoddy stick figure on top.
+                <br/>
+            </p>
+            <Note>Because my shader's object perfectly fits the camera, the artificial scene looks like the real one.</Note>
+            <H2>Ray Tracing 101</H2>
             <IMG
                 src='/blogPics/badRayTracing.png'
                 height={687}
                 width={683}
-                alt='My Fragment Shader'
+                alt='Bad ray tracing'
             >
                 The bad approach for ray tracing.
             </IMG>
@@ -155,25 +168,41 @@ function blogTSX(props: any) {
                 src='/blogPics/goodRayTracing.png'
                 height={687}
                 width={683}
-                alt='My Fragment Shader'
+                alt='Good ray tracing'
             >
                 The better approach for ray tracing. No redundant rays!
             </IMG>
             <Note>Because we are simulating rays moving backwards, this is sometimes called backwards ray tracing.</Note>
-            <H3>2. Depth Texture</H3>
+            <H2>Depth Texture</H2>
+            <IMG
+                src='/blogPics/depth.png'
+                height={680}
+                width={680}
+                alt='Motivation for depth texture'
+            >
+                Two rays casted into an atmosphere.
+            </IMG>
             <p>
-                Remember when I said there were only two renderings? I LIED. There's technically three. The computations necessary for my atmosphere require many inputs, and one of these is a
-                texture where each pixel indicates how far away the object in that pixel is away from the camera. Pixels of close objects are usually black, whereas far objects appear white.
-                This depth texture is rendered along with the first frame, the one that the fragment shader draws on top of.
+                There's another critical part of the atmosphere we're forgetting. The further a ray travels into the atmosphere, the more the atmosphere should contribute
+                to the color of the pixel. Take a look at the photo above.
                 <br/><br/>
-                Using this texture of distances, we can see how far a ray of light "emitted" from any pixel would reach into the atmosphere. Intuitively, the further a ray of light
-                travels in the atmosphere, the greater the chance is to see any scattered sunlight.
+                The ray on the left doesn't travel far into the atmosphere, because it hits the top of the mountain. The right ray doesn't, and travels much further into denser parts
+                of the atmosphere. We should expect that the pixel corresponding to the left ray should have a lot less atmospheric color than the ray on the right.
+                <br/><br/>
+                However, determining the path our ray travels into the atmosphere, we need a measurement of depth from the environment. A depth texture tell us how far each pixel
+                reaches into the scene we are rendering.
             </p>
-            <H3>3. Cheesing Physics</H3>
+            <H2>The Physics</H2>
             <p>
-                There are a whole bunch of integrals used to compute the exact values of light rays that would reach our camera. Unfortunately, our computer doesn't have the resources to
-                perform these integrations. Instead, we can write <Code>for</Code> loops to determine the Riemann sum appoximations.
+                As it turns out, the equations for determining how much atmospheric color should be added to a pixel are rather intimidating.
                 <br/><br/>
+                In a perfect world, integrals would be used to compute the exact values of light rays that would reach our camera. Although, our computer doesn't have the ability to
+                evaluate them. Instead, we can write <Code>for</Code> loops to determine the Riemann sum appoximations.
+            </p>
+            <Important>
+                The name of this process is called numeric integration, and it may be an important part of future blogs!
+            </Important>
+            <p>
                 For more details on the math behind atmospheric scattering, check out
                 Nvidia's <a className='text-blue-400'
                 href="https://developer.nvidia.com/gpugems/gpugems2/part-ii-shading-lighting-and-shadows/chapter-16-accurate-atmospheric-scattering"><u>guide</u></a> and Sebastian
