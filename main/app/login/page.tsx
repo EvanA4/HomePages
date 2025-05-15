16fe88ba8a4ddb68fe8459d7fc4519ad06f2bdee
Evan Abbott <107959883+EvanA4@users.noreply.github.com>
2025-05-15 10:11:22 -0400

    added page verification layout

--- main/app/login/page.tsx
+++ main/app/login/page.tsx
@@ -5,6 +5,8 @@ import { Nav } from "../components/nav";
 import { getHint, useUser, validateCredentials } from "@/public/utils/authUtils";
 import { actionLogin } from "@/actions/authActions";
 import Image from "next/image";
+import { User } from "@/types/authtypes";
+import VerifyLayout, { PageVerifyType } from "../components/verifyLayout";
 
 export default function Login() {
     const [inputUsername, setUsername] = useState("");
@@ -13,13 +15,28 @@ export default function Login() {
     const [showPassword, setShowPassword] = useState(false);
     const [passwordHint, setPasswordHint] = useState("");
 
-    const { user, loading } = useUser();
+    const { user } = useUser();
     const router = useRouter();
     useEffect(() => {
         if (user) {
             router.push("/");
         }
     }, [user]);
+
+    function verify(user: User | undefined): PageVerifyType {
+        const pv: PageVerifyType = {
+            accepted: false,
+            rejectMsg: "You cannot be logged in to visit this page.",
+            url: "/",
+        }
+
+        if (user) {
+            return pv;
+        } else {
+            pv.accepted = true;
+            return pv;
+        }
+    }
 
     async function handleSubmit() {
         // console.log(`inputUsername: \"${inputUsername}\"`);
@@ -64,73 +81,75 @@ export default function Login() {
         <div className="w-full relative">
             <Nav alwaysOn={true} />
 
-            <div className="h-[100vh] w-full flex flex-col items-center justify-center text-white">
-                <div className="flex flex-col items-center justify-center p-5 rounded-lg bg-neutral-950 bg-opacity-20 w-[300px]">
-                    <p className="text-3xl mb-10">Login</p>
+            <VerifyLayout verify={verify}>
+                <div className="h-[100vh] w-full flex flex-col items-center justify-center text-white">
+                    <div className="flex flex-col items-center justify-center p-5 rounded-lg bg-neutral-950 bg-opacity-20 w-[300px]">
+                        <p className="text-3xl mb-10">Login</p>
 
-                    <div className="p-2">
-                        <label htmlFor="username" className="mr-3 block">Username</label>
-                        <input
-                            value={inputUsername}
-                            onChange={e => setUsername(e.target.value)}
-                            type="text"
-                            name="username"
-                            id="username"
-                            className="bg-white outline-none rounded-lg placeholder-neutral-400 px-2 py-1 text-black"
-                            placeholder="username"
-                        />
-                        <br />
-                    </div>
-
-                    <div className="p-2">
-                        <label htmlFor="password" className="mr-3 block">Password</label>
-                        <div className="relative inline">
+                        <div className="p-2">
+                            <label htmlFor="username" className="mr-3 block">Username</label>
                             <input
-                                value={inputPassword}
-                                onChange={e => setPassword(e.target.value)}
-                                type={showPassword ? "text" : "password"}
-                                name="password"
-                                id="password"
+                                value={inputUsername}
+                                onChange={e => setUsername(e.target.value)}
+                                type="text"
+                                name="username"
+                                id="username"
                                 className="bg-white outline-none rounded-lg placeholder-neutral-400 px-2 py-1 text-black"
-                                placeholder="password"
-                                content="hide"
+                                placeholder="username"
                             />
+                            <br />
+                        </div>
+
+                        <div className="p-2">
+                            <label htmlFor="password" className="mr-3 block">Password</label>
+                            <div className="relative inline">
+                                <input
+                                    value={inputPassword}
+                                    onChange={e => setPassword(e.target.value)}
+                                    type={showPassword ? "text" : "password"}
+                                    name="password"
+                                    id="password"
+                                    className="bg-white outline-none rounded-lg placeholder-neutral-400 px-2 py-1 text-black"
+                                    placeholder="password"
+                                    content="hide"
+                                />
+
+                                <button
+                                    onClick={() => setShowPassword(prev => !prev)}
+                                    className="absolute top-[50%] right-0 -translate-y-[50%] mr-2 opacity-70 hover:opacity-100"
+                                >
+                                    <Image
+                                        src={showPassword ? "/svgs/show.svg" : "/svgs/hide.svg"}
+                                        height={20}
+                                        width={20}
+                                        alt="Hide icon"
+                                    />
+                                </button>
+                            </div>
+                        </div>
+
+                        {passwordHint && <p>Hint: <i className="text-neutral-400">{passwordHint}</i></p>}
+
+                        {globalError && <p className="text-red-500">{globalError}</p>}
+
+                        <div className="flex gap-3">
+                            <button
+                                onClick={handleSubmit}
+                                className="px-3 py-2 bg-blue-600 hover:bg-blue-700 rounded-lg mt-6"
+                            >Submit</button>
 
                             <button
-                                onClick={() => setShowPassword(prev => !prev)}
-                                className="absolute top-[50%] right-0 -translate-y-[50%] mr-2 opacity-70 hover:opacity-100"
-                            >
-                                <Image
-                                    src={showPassword ? "/svgs/show.svg" : "/svgs/hide.svg"}
-                                    height={20}
-                                    width={20}
-                                    alt="Hide icon"
-                                />
-                            </button>
+                                onClick={handleHint}
+                                className="px-3 py-2 bg-orange-600 hover:bg-orange-700 rounded-lg mt-6"
+                            >Hint</button>
                         </div>
+
+                        <p>or</p>
+
+                        <a href="/register" className="text-blue-500 hover:text-blue-400">Create an account</a>
                     </div>
-
-                    {passwordHint && <p>Hint: <i className="text-neutral-400">{passwordHint}</i></p>}
-
-                    {globalError && <p className="text-red-500">{globalError}</p>}
-
-                    <div className="flex gap-3">
-                        <button
-                            onClick={handleSubmit}
-                            className="px-3 py-2 bg-blue-600 hover:bg-blue-700 rounded-lg mt-6"
-                        >Submit</button>
-
-                        <button
-                            onClick={handleHint}
-                            className="px-3 py-2 bg-orange-600 hover:bg-orange-700 rounded-lg mt-6"
-                        >Hint</button>
-                    </div>
-
-                    <p>or</p>
-
-                    <a href="/register" className="text-blue-500 hover:text-blue-400">Create an account</a>
                 </div>
-            </div>
+            </VerifyLayout>
         </div>
     )
 }
--- main/app/register/page.tsx
+++ main/app/register/page.tsx
@@ -5,6 +5,8 @@ import { createUser } from "@/public/utils/authUtils";
 import { actionLogin } from "@/actions/authActions";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
+import { User } from "@/types/authtypes";
+import VerifyLayout, { PageVerifyType } from "../components/verifyLayout";
 
 export default function Login() {
     const [inputUsername, setUsername] = useState("");
@@ -13,6 +15,21 @@ export default function Login() {
     const [globalError, setGlobalError] = useState("");
     const [showPassword, setShowPassword] = useState(false);
     const router = useRouter();
+
+    function verify(user: User | undefined): PageVerifyType {
+        const pv: PageVerifyType = {
+            accepted: false,
+            rejectMsg: "You cannot be logged in to visit this page.",
+            url: "/",
+        }
+
+        if (user) {
+            return pv;
+        } else {
+            pv.accepted = true;
+            return pv;
+        }
+    }
 
     async function handleSubmit() {
         // console.log(`inputUsername: \"${inputUsername}\"`);
@@ -64,77 +81,79 @@ export default function Login() {
         <div className="w-full">
             <Nav alwaysOn />
 
-            <div className="h-[100vh] w-full flex flex-col items-center justify-center text-white">
-                <div className="flex flex-col items-center justify-center p-5 rounded-lg bg-neutral-950 bg-opacity-20 w-[300px]">                    
-                    <p className="text-3xl mb-10">Register</p>
+            <VerifyLayout verify={verify}>
+                <div className="h-[100vh] w-full flex flex-col items-center justify-center text-white">
+                    <div className="flex flex-col items-center justify-center p-5 rounded-lg bg-neutral-950 bg-opacity-20 w-[300px]">                    
+                        <p className="text-3xl mb-10">Register</p>
 
-                    <div className="p-2">
-                        <label htmlFor="username" className="mr-3 block">Username</label>
-                        <input
-                            value={inputUsername}
-                            onChange={e => setUsername(e.target.value)}
-                            type="text"
-                            name="username"
-                            id="username"
-                            className="bg-white outline-none rounded-lg placeholder-neutral-400 px-2 py-1 text-black"
-                            placeholder="username"
-                        />
-                        <br />
-                    </div>
-
-                    <div className="p-2">
-                        <label htmlFor="password" className="mr-3 block">Password</label>
-                        <div className="relative inline">
+                        <div className="p-2">
+                            <label htmlFor="username" className="mr-3 block">Username</label>
                             <input
-                                value={inputPassword}
-                                onChange={e => setPassword(e.target.value)}
-                                type={showPassword ? "text" : "password"}
-                                name="password"
-                                id="password"
+                                value={inputUsername}
+                                onChange={e => setUsername(e.target.value)}
+                                type="text"
+                                name="username"
+                                id="username"
                                 className="bg-white outline-none rounded-lg placeholder-neutral-400 px-2 py-1 text-black"
-                                placeholder="password"
-                                content="hide"
+                                placeholder="username"
                             />
-
-                            <button
-                                onClick={() => setShowPassword(prev => !prev)}
-                                className="absolute top-[50%] right-0 -translate-y-[50%] mr-2 opacity-70 hover:opacity-100"
-                            >
-                                <Image
-                                    src={showPassword ? "/svgs/show.svg" : "/svgs/hide.svg"}
-                                    height={20}
-                                    width={20}
-                                    alt="Hide icon"
-                                />
-                            </button>
+                            <br />
                         </div>
+
+                        <div className="p-2">
+                            <label htmlFor="password" className="mr-3 block">Password</label>
+                            <div className="relative inline">
+                                <input
+                                    value={inputPassword}
+                                    onChange={e => setPassword(e.target.value)}
+                                    type={showPassword ? "text" : "password"}
+                                    name="password"
+                                    id="password"
+                                    className="bg-white outline-none rounded-lg placeholder-neutral-400 px-2 py-1 text-black"
+                                    placeholder="password"
+                                    content="hide"
+                                />
+
+                                <button
+                                    onClick={() => setShowPassword(prev => !prev)}
+                                    className="absolute top-[50%] right-0 -translate-y-[50%] mr-2 opacity-70 hover:opacity-100"
+                                >
+                                    <Image
+                                        src={showPassword ? "/svgs/show.svg" : "/svgs/hide.svg"}
+                                        height={20}
+                                        width={20}
+                                        alt="Hide icon"
+                                    />
+                                </button>
+                            </div>
+                        </div>
+
+                        <div className="p-2">
+                            <label htmlFor="hint" className="mr-3 block">Hint</label>
+                            <input
+                                value={inputHint}
+                                onChange={e => setHint(e.target.value)}
+                                type="text"
+                                name="hint"
+                                id="hint"
+                                className="bg-white outline-none rounded-lg placeholder-neutral-400 px-2 py-1 text-black"
+                                placeholder="hint"
+                            />
+                        </div>
+
+                        {globalError && <p className="text-red-500">{globalError}</p>}
+
+                        <button
+                            onClick={handleSubmit}
+                            className="px-3 py-2 bg-blue-600 hover:bg-blue-700 rounded-lg mt-6"
+                        >Submit</button>
+
+                        <p>or</p>
+
+                        <a href="/login" className="text-blue-500 hover:text-blue-400">Sign in</a>
                     </div>
-
-                    <div className="p-2">
-                        <label htmlFor="hint" className="mr-3 block">Hint</label>
-                        <input
-                            value={inputHint}
-                            onChange={e => setHint(e.target.value)}
-                            type="text"
-                            name="hint"
-                            id="hint"
-                            className="bg-white outline-none rounded-lg placeholder-neutral-400 px-2 py-1 text-black"
-                            placeholder="hint"
-                        />
-                    </div>
-
-                    {globalError && <p className="text-red-500">{globalError}</p>}
-
-                    <button
-                        onClick={handleSubmit}
-                        className="px-3 py-2 bg-blue-600 hover:bg-blue-700 rounded-lg mt-6"
-                    >Submit</button>
-
-                    <p>or</p>
-
-                    <a href="/login" className="text-blue-500 hover:text-blue-400">Sign in</a>
                 </div>
-            </div>
+            </VerifyLayout>
         </div>
     )
 }
